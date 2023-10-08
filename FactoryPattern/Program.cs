8b9b2d075f48959ad044acab98d6473a623450d2
Felix Adnel <felixadnel@Felixs-MBP.lan>
2023-10-08 00:12:19 -0500

    fixed loop

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -13,7 +13,7 @@ namespace FactoryPattern
                 Console.WriteLine("Enter the ammount of tires for the vehicle you want to create:");
 
                 input = int.TryParse(Console.ReadLine(), out numOfWheels);
-            } while (!input == false);
+            } while (!input);
 
             var vehicle = VehicleFactory.GetVehicle(numOfWheels);
             vehicle.Drive();
--- FactoryPattern/VehicleFactory.cs
+++ FactoryPattern/VehicleFactory.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 namespace FactoryPattern
 {
-	public static class VehicleFactory
-	{
-		public static IVehicle GetVehicle(int wheelCount)
-		{
-			switch (wheelCount)
-			{
-				case 2:
-					return new Motorcycle();
-				case 3:
-					return new Threewheeler();
-				case 4:
-					return new Car();
-				default:
-					return new Car();
-			}
-		}
-	}
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelCount)
+        {
+            switch (wheelCount)
+            {
+                case 2:
+                    return new Motorcycle();
+                case 3:
+                    return new Threewheeler();
+                case 4:
+                    return new Car();
+                default:
+                    Console.WriteLine("Unrecognized number of wheels! Defaulting to Car.");
+                    return new Car();
+            }
+        }
+    }
+
 }
 
